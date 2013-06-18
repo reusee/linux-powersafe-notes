92c9971b2a27b7f5fd625fc69fadd383887f2c32
reus <reusee@ymail.com>
2013-06-18 14:35:45 +0800

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 linux-powersafe-notes
 =====================
+
+kernel.watchdog = 0
+vm.dirty_writeback_centisecs = 1500
---
+++ powersave.sh
@@ -0,0 +1,18 @@
+for i in 0 1 2 3 4 5 ; do
+  echo min_power>/sys/class/scsi_host/host${i}/link_power_management_policy
+done
+
+for i in /sys/bus/pci/devices/*/power/control ; do
+  echo auto > ${i}
+done
+
+for i in /sys/bus/usb/devices/*/power/control ; do
+  echo auto > ${i}
+done
+
+for i in /sys/devices/system/cpu/cpu?/cpufreq/scaling_governor ; do
+  echo ondemand > ${i}
+done
+
+echo 5 > /sys/module/snd_hda_intel/parameters/power_save
+#echo N > /sys/module/snd_hda_intel/parameters/power_save_controller
